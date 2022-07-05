1fe3871e45660f81876b3d4eb9c6ed2077c8127d
yuki-shi <88805836+yuki-shi@users.noreply.github.com>
2022-07-04 22:08:28 -0300

    Delete data-cleaning.py

--- data-cleaning.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv')
-df.rename(columns={'#':'Id', 'Type 1':'Type1', 'Type 2':'Type2', 'Sp. Atk':'Sp.Atk', 'Sp. Def': 'Sp.Def'}, inplace=True)
-df = df[~df['Name'].str.contains('Mega')]
-df.to_csv('pokemon.csv', index=False)
