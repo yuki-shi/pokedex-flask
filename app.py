43fbce6a1c2a3f9ca4b993798cb637de83d8d903
yuki-shi <88805836+yuki-shi@users.noreply.github.com>
2022-07-06 01:54:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,38 +1,71 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import sqlite3
 import plotly
 import plotly.graph_objects as go
 import json
+from pg_auth import pg_auth
+import re
 
 app = Flask(__name__)
 
-#---
-@app.route('/', methods=['GET', 'POST'])
+#---------
+
+@app.route('/')
 def index():
 
-    connection = sqlite3.connect('database.db')
-    df = pd.read_sql_query("""
-        SELECT * FROM pokemon""", con=connection)
-    connection.commit()
-    connection.close()
+    conn = pg_auth()
+    cursor = conn.cursor()
 
-    df = df.head(30)
+    cursor.execute("""SELECT DISTINCT type1 FROM pokemon;""")
+    columns = cursor.fetchall()
+    cursor.execute("""SELECT DISTINCT type2 FROM pokemon;""")
+    columns.extend(cursor.fetchall())
+
+    columns = set(columns) # remover duplicados
+
+    types = [re.sub(r"[,()']", '', str(x)) for x in columns]
+    types.remove('None')
+    types.sort()
+
+    return render_template('index.html', types=types)
+
+
+@app.route('/callback')
+def callback():
+    return get_graph(request.args.get('data'))
+
+
+
+#---------
+def get_graph(type='grass'):
+
+    conn = pg_auth()
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT * FROM pokemon
+         WHERE type1 = '{type}'
+         OR type2 = '{type}'; """)
+
+    columns = [x[0] for x in cursor.description]     
+    rows = cursor.fetchall()
+    df = pd.DataFrame(rows, columns=columns)
+
+    conn.close()
+    cursor.close
 
     fig = go.Figure(data=[go.Table(
                             header=dict(values=list(df.columns),
                                         fill_color='olive',
                                         align='left'),
                             cells=dict(values=df.transpose(),
-                                       align='left'))
+                                    align='left'))
                     ])
-
-
+    fig.update_layout(width=980, height=1900)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('index.html', graphJSON=graphJSON)
+    return graphJSON
 
-
-#---
+#------------
 if __name__ == '__main__':
     app.run(debug=True)
